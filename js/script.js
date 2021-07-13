a20c69ca96e7a28f357a0e834e408e587c4770ae
UvereAnn <uvereann@gmail.com>
2021-07-13 02:16:09 -0700

    Second Commit: Added Partners, footer, logo and about us sessions

--- js/script.js
+++ js/script.js
@@ -11,3 +11,30 @@ function scrollFunction(){
         document.getElementById("navbar").style.display = "block"
     }
 }
+
+
+/**************  Slick slider         ***************/
+
+$(document).ready(function(){
+    $('.partners-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breaakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+
+        }, {
+            breaakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
