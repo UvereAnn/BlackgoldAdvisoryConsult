5f99d4fa46633944a1e73b40dbfdb8b8fbf59f22
UvereAnn <uvereann@gmail.com>
2021-07-14 12:38:04 -0700

    Added more styling and Trending Session

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
 
 function scrollFunction(){
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
-        document.getElementById("navba").style.backgroundColor = "#333"
+        document.getElementById("navba").style.backgroundColor = "black"
         document.getElementById("navbar").style.display = "none"
     }else{
         document.getElementById("navba").style.backgroundColor = "#3330"
